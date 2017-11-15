699f63e3a234da54f91cfb9f1f0872f953a76ac3
ronsaran2009 <59070164@it.kmitl.ac.th>
2017-11-15 15:03:04 +0700

    fix bug Selection dark

--- core/src/Element/Score.java
+++ core/src/Element/Score.java
@@ -14,16 +14,16 @@ public class Score extends Actor {
     String pic[] = new String[10];//{"num0.png", "num1.png", "num2.png", "num3.png", "num4.png", "num5.png", "num6.png", "num7.png", "num8.png", "num9.png"};
     public Score(int num , float x, float y, float w, float h){
 
-        pic[0] = "num0.png";
-        pic[1] = "num1.png";
-        pic[2] = "num2.png";
-        pic[3] = "num3.png";
-        pic[4] = "num4.png";
-        pic[5] = "num5.png";
-        pic[6] = "num6.png";
-        pic[7] = "num7.png";
-        pic[8] = "num8.png";
-        pic[9] = "num9.png";
+        pic[0] = "comic-boom-explosion-2-7.png";
+        pic[1] = "comic-boom-explosion-2-7.png";
+        pic[2] = "comic-boom-explosion-2-7.png";
+        pic[3] = "comic-boom-explosion-2-7.png";
+        pic[4] = "comic-boom-explosion-2-7.png";
+        pic[5] = "comic-boom-explosion-2-7.png";
+        pic[6] = "comic-boom-explosion-2-7.png";
+        pic[7] = "comic-boom-explosion-2-7.png";
+        pic[8] = "comic-boom-explosion-2-7.png";
+        pic[9] = "comic-boom-explosion-2-7.png";
 
         if (num == 0){
             name = pic[0];
