6bf56d40ac96d47afe0d4c603a2eb7bce821f01a
ronsaran2009 <59070164@it.kmitl.ac.th>
2017-11-19 17:08:47 +0700

    clo

--- core/src/Element/SideObj.java
+++ core/src/Element/SideObj.java
@@ -6,235 +6,239 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class SideObj extends Actor {
-	public Texture SideObj;
-	public float x;
-	public float y;
-	public float w;
-	public float h;
-	public int check;
-	Rectangle rect;
-	public boolean brakedown = false;
-	public boolean brakedownround1;
-	public int limitfloor;
-	public int level;
-	public int random;
+    public Texture SideObj;
+    public float x ;
+    public float y ;
+    public float w ;
+    public float h ;
+    public int check;
+    Rectangle rect;
+    public boolean brakedown = false;
+    public boolean brakedownround1 ;
+    int limitfloor;
+    int level;
+    int random;
 
-	public SideObj(int random, int level) {
-		this.level = level;
-		if (level == 0) {// ez
-			if (random == 0) { // left
-				SideObj = new Texture("h4-2l.png");
-				this.x = 0;
-				this.y = 850;
-				this.w = 400;
-				this.h = 200;
-				this.check = 0;
-			} else if (random == 1) { // center
-				SideObj = new Texture("h4-2.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 1;
-			} else if (random == 2) { // right
-				SideObj = new Texture("h4-2r.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 400;
-				this.h = 200;
-				this.check = 2;
-			} else if (random == 3) { // end
-				SideObj = new Texture("r4.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 3;
-			} else if (random == 4) { // Start
-				SideObj = new Texture("h4.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 4;
-			}
-		}
-		//////////////////////////////////////////////////////
-		else if (level == 1) {// normal
-			if (random == 0) { // left
-				SideObj = new Texture("h1-2l.png");
-				this.x = 0;
-				this.y = 850;
-				this.w = 400;
-				this.h = 200;
-				this.check = 0;
-			} else if (random == 1) { // center
-				SideObj = new Texture("h1-2.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 1;
-			} else if (random == 2) { // right
-				SideObj = new Texture("h1-2r.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 400;
-				this.h = 200;
-				this.check = 2;
-			} else if (random == 3) { // end
-				SideObj = new Texture("r1.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 3;
-			} else if (random == 4) { // Start
-				SideObj = new Texture("h1.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 4;
-			}
-		}
-		//////////////////////////////////////////////////////
-		else if (level == 2) {// hard
-			if (random == 0) { // left
-				SideObj = new Texture("h2-2l.png");
-				this.x = 0;
-				this.y = 850;
-				this.w = 400;
-				this.h = 200;
-				this.check = 0;
-			} else if (random == 1) { // center
-				SideObj = new Texture("h2-2.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 1;
-			} else if (random == 2) { // right
-				SideObj = new Texture("h2-2r.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 400;
-				this.h = 200;
-				this.check = 2;
-			} else if (random == 3) { // end
-				SideObj = new Texture("r2.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 3;
-			} else if (random == 4) { // Start
-				SideObj = new Texture("h2.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 4;
-			}
-		}
-		//////////////////////////////////////////////////////
-		else if (level == 3) {// free
-			if (random == 0) { // left
-				SideObj = new Texture("h3-2l.png");
-				this.x = 0;
-				this.y = 850;
-				this.w = 400;
-				this.h = 200;
-				this.check = 0;
-			} else if (random == 1) { // center
-				SideObj = new Texture("h3.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 1;
-			} else if (random == 2) { // right
-				SideObj = new Texture("h3-2r.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 400;
-				this.h = 200;
-				this.check = 2;
-			} else if (random == 3) { // end
-				SideObj = new Texture("r3.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 3;
-			} else if (random == 4) { // Start
-				SideObj = new Texture("h3.png");
-				this.x = 200;
-				this.y = 850;
-				this.w = 200;
-				this.h = 200;
-				this.check = 4;
-			}
+    public SideObj(int random, int level){
+        this.level = level;
+        if (level == 0) {//ez
+            if (random == 0) { // left
+                SideObj = new Texture("h4-2l.png");
+                this.x = 0;
+                this.y = 850;
+                this.w = 400;
+                this.h = 200;
+                this.check = 0;
+            } else if (random == 1) { // center
+                SideObj = new Texture("h4-2.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 1;
+            } else if (random == 2) { // right
+                SideObj = new Texture("h4-2r.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 400;
+                this.h = 200;
+                this.check = 2;
+            } else if (random == 3) { // end
+                SideObj = new Texture("r4.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 3;
+            } else if (random == 4) { // Start
+                SideObj = new Texture("h4.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 4;
+            }
+        }
+        //////////////////////////////////////////////////////
+        else if (level == 1) {//normal
+            if (random == 0) { // left
+                SideObj = new Texture("h1-2l.png");
+                this.x = 0;
+                this.y = 850;
+                this.w = 400;
+                this.h = 200;
+                this.check = 0;
+            } else if (random == 1) { // center
+                SideObj = new Texture("h1-2.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 1;
+            } else if (random == 2) { // right
+                SideObj = new Texture("h1-2r.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 400;
+                this.h = 200;
+                this.check = 2;
+            } else if (random == 3) { // end
+                SideObj = new Texture("r1.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 3;
+            } else if (random == 4) { // Start
+                SideObj = new Texture("h1.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 4;
+            }
+        }
+        //////////////////////////////////////////////////////
+        else if (level == 2) {//hard
+            if (random == 0) { // left
+                SideObj = new Texture("h2-2l.png");
+                this.x = 0;
+                this.y = 850;
+                this.w = 400;
+                this.h = 200;
+                this.check = 0;
+            } else if (random == 1) { // center
+                SideObj = new Texture("h2-2.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 1;
+            } else if (random == 2) { // right
+                SideObj = new Texture("h2-2r.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 400;
+                this.h = 200;
+                this.check = 2;
+            } else if (random == 3) { // end
+                SideObj = new Texture("r2.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 3;
+            } else if (random == 4) { // Start
+                SideObj = new Texture("h2.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 4;
+            }
+        }
+        //////////////////////////////////////////////////////
+        else if (level == 3) {//free
+            if (random == 0) { // left
+                SideObj = new Texture("h3-2l.png");
+                this.x = 0;
+                this.y = 850;
+                this.w = 400;
+                this.h = 200;
+                this.check = 0;
+            } else if (random == 1) { // center
+                SideObj = new Texture("h3.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 1;
+            } else if (random == 2) { // right
+                SideObj = new Texture("h3-2r.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 400;
+                this.h = 200;
+                this.check = 2;
+            } else if (random == 3) { // end
+                SideObj = new Texture("r3.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 3;
+            } else if (random == 4) { // Start
+                SideObj = new Texture("h3.png");
+                this.x = 200;
+                this.y = 850;
+                this.w = 200;
+                this.h = 200;
+                this.check = 4;
+            }
 
-		}
-		rect = new Rectangle(this.x, this.y, this.w, this.h);
-	}
+                }rect = new Rectangle(this.x, this.y, this.w, this.h);
+            }
 
-	public void setLimitfloor(int limitfloor) {
-		this.limitfloor = limitfloor;
-	}
 
-	public void draw(Batch batch, float alpha) {
-		// y -= 200;
-		if (brakedown == true) {
-			if (y == 50) {
-				brakedown = false;
-				brakedownround1 = false;
-			} else if (y == 250) {
-				brakedown = false;
-				brakedownround1 = false;
-			} else if (y == 450) {
-				brakedown = false;
-				brakedownround1 = false;
-			} else if (y == 650) {
-				brakedown = false;
-				brakedownround1 = false;
-			} else {
-				y -= 25;
-				brakedownround1 = true;
-			}
-		}
-		batch.draw(SideObj, x, y, w, h);
-		rect.setY(y);
-	}
+    public void setLimitfloor(int limitfloor) {
+        this.limitfloor = limitfloor;
+    }
 
-	public boolean isBrakedownround1() {
-		return brakedownround1;
-	}
-
-	public void setBreakdown() {
-
-	}
-
-	public void setRandom(int random) {
-		this.random = random;
-	}
-
-	public String getcenter() {
-		String name = "";
-		if (this.level == 0) {
-			name = "h4-2.png";
-		} else if (this.level == 1) {
-			name = "h1-2.png";
-		} else if (this.level == 2) {
-			name = "h2-2.png";
-		} else if (this.level == 3) {
-			name = "h3.png";
-		}
-		return name;
-	}
-
-	public void dispose() {
-		SideObj.dispose();
-	}
+    public void draw(Batch batch, float alpha){
+        //y -= 200;
+    	if(brakedown == true){
+            if (y == 50 ){
+                brakedown = false;
+                brakedownround1 = false;
+            }
+            else if (y == 250 ){
+                brakedown = false;
+                brakedownround1 = false;
+            }
+            else if (y == 450 ){
+                brakedown = false;
+                brakedownround1 = false;
+            }
+            else if (y == 650 ){
+                brakedown = false;
+                brakedownround1 = false;
+            }
+            else{
+                y -= 25;
+                brakedownround1 = true;
+            }
+        }
+        batch.draw(SideObj, x, y, w, h);
+        rect.setY(y);
+    }
+    
+    public boolean isBrakedownround1() {
+        return brakedownround1;
+    }
+    
+    public void setBreakdown() {
+    	
+    }
+    
+    public void setRandom(int random) {
+    	this.random = random;
+    }
+    
+    public String getcenter() {
+    	String name = "";
+    	if(this.level == 0) {
+    		name = "h4-2.png";
+    	}else if(this.level == 1) {
+    		name = "h1-2.png";
+    	}else if(this.level == 2) {
+    		name = "h2-2.png";
+    	}else if(this.level == 3) {
+    		name = "h3.png";
+    	}
+    	return name;
+    }
+    
+    public void dispose() {
+    	SideObj.dispose();
+    }
 }
--- core/src/Element/Timer.java
+++ core/src/Element/Timer.java
@@ -5,80 +5,82 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public class Timer extends Actor {
+public class Timer extends Actor{
 
-	Texture bar;
-	Texture minBar;
-	Texture midBar;
-	Texture fullBar;
-	public float x;
-	public float y;
-	public float w;
-	public float fixw;// final w use for calculate %.
-	public float h;
-	public float timer;// final time.
-	public float time;
-	public float mid = 150;
-	public float min = 50;
-	public boolean end = true;
 	public boolean check = false;
+    Texture bar;
+    Texture minBar;
+    Texture midBar;
+    Texture fullBar;
+    float x ;
+    float y ;
+    float w ;
+    float fixw;// final w use for calculate %.
+    float h ;
+    float timer;// final time.
+    public float time;
+    float mid = 150;
+    float min = 50;
+    boolean end = true;
 
-	public Timer(float time) {
-		bar = new Texture("fulltimebar.png");
-		minBar = new Texture("mintimebar.png");
-		midBar = new Texture("medtimebar.png");
-		fullBar = new Texture("fulltimebar.png");
-		this.x = 82;
-		this.y = 750;
-		this.w = 500;
-		this.h = 25;
-		this.fixw = this.w;
-		this.time = time;
-		this.timer = time;
+    public Timer (float time){
+        bar = new Texture("fulltimebar.png");
+        minBar = new Texture("mintimebar.png");
+        midBar = new Texture("medtimebar.png");
+        fullBar = new Texture("fulltimebar.png");
+        this.x = 82;
+        this.y = 750;
+        this.w = 500;
+        this.h = 25;
+        this.fixw = this.w;
+        this.time = time;
+        this.timer = time;
 
-	}
 
-	public void draw(Batch batch, float alpha) {
-		if (check == true) {
-			time -= Gdx.graphics.getDeltaTime();
-			w = (time / timer) * fixw; // find % of bar.
+    }
+    public void draw(Batch batch, float alpha){
+        if (check == true){
+            time -= Gdx.graphics.getDeltaTime();
+            w = (time/timer)*fixw; // find % of bar.
 
-			if (w <= min) {
-				bar = minBar;// Red bar.
-				// System.out.println("red : "+time);
-			} else if (w <= mid) {
-				bar = midBar;// Yellow bar.
-				// System.out.println("Yello : "+time);
-			} else if (w <= fixw) {
-				bar = fullBar;// Green bar.
-			}
-			if (w >= fixw) { // if bar over maximum.
-				w = fixw;
-				time = timer;
-			}
+            if (w <= min) {
+                bar = minBar;// Red bar.
+                //System.out.println("red : "+time);
+            }
+            else if (w <= mid){
+                bar = midBar;// Yellow bar.
+                //System.out.println("Yello : "+time);
+            }
+            else if (w <= fixw){
+                bar = fullBar;// Green bar.
+            }
+            if (w >= fixw){ // if bar over maximum.
+                w = fixw;
+                time = timer;
+            }
 
-			if (time <= 0) { // Time out.
-				check = false;
-				end = false;
-			}
-		}
-		batch.draw(bar, x, y, w, h);
+            if (time <= 0){ // Time out.
+                check = false;
+                end = false;
+            }
+        }
+        batch.draw(bar, x,y, w, h);
 
-	}
+    }
 
-	public float getTime() {
-		return time;
-	}
+    public float getTime() {
+        return time;
+    }
 
-	public void setCheck(boolean x) {
-		check = x;
-	}
+    public void setCheck(boolean x) {
+    	check = x;
+    }
 
-	public boolean isEnd() {
-		return end;
-	}
-
-	public void dispose() {
-		bar.dispose();
-	}
+    public boolean isEnd() {
+        return end;
+    }
+    
+    public void dispose() {
+    	bar.dispose();
+    }
 }
